66524956efd178584ef755b3a50a26e844d3aa30
Matthew Fors <1824866+mattfors@users.noreply.github.com>
2024-06-01 10:29:52 -0400

    feature: wire lib to showcase app

--- projects/showcase/src/app/app.component.ts
+++ projects/showcase/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelsinkiUiComponent } from '../../../helsinki-ui/src';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelsinkiUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
